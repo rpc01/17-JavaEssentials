3b72c85a571e0d4b3da85c18647f741284c73049
rpc01 <rpratc@gmail.com>
2018-06-14 09:16:53 +0200

    Exercise

--- Exercise Files/trials/src/trials/arrayToString.java
+++
@@ -1,5 +0,0 @@
-package trials;
-
-public class arrayToString {
-
-}
--- packages/com/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-  public static void main(String[] args){
-    System.out.println("hi");
-    System.out.println(args[0]);
-    System.out.println(args[2]);
-  }
-
-}
