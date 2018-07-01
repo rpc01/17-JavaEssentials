a01ec92da602bc01b23ee7ffc01fb348fe8e8817
rpc01 <rpratc@gmail.com>
2018-07-02 00:25:21 +0200

    summary

---
+++ src/JavaEssentials.java
@@ -0,0 +1,39 @@
+
+public class JavaEssentials {
+
+		
+		public static void main(String[] args) {
+			System.out.println("Hello from Java!");
+			System.out.println(args[0]);
+			
+
+			    static boolean bDef;
+
+
+			        boolean b1 = true;
+			        boolean b2 = false;
+
+			        System.out.println("The value of b1 is: " + b1);
+			        System.out.println("The value of b2 is: " + b2);
+			        System.out.println("The value of bDef is: " + bDef);
+
+			        boolean b3 = !b1;
+			        System.out.println("The value of b3 is: " + b3);
+
+			        int i1 = 0;
+			        boolean b4 = (i1 != 0);
+			        System.out.println("The value of b4 is: " + b4);
+
+			        String sBoolean = "true";
+			        boolean parsed = Boolean.parseBoolean(sBoolean);
+			        System.out.println("Parsed: " + parsed);
+
+
+			}
+			
+			
+		}
+
+	
+	
+}
